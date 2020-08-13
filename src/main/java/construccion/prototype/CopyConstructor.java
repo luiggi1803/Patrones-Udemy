c872bf471c7976661bcb0a6480c2014e1eff1433
luiggi huaman <luiggi.huaman.18@gmail.com>
2020-08-12 22:12:40 -0500

    Add Prototype

---
+++ src/main/java/construccion/prototype/CopyConstructor.java
@@ -0,0 +1,65 @@
+package construccion.prototype;
+
+class Address {
+
+    private String streetAddress;
+    private String city;
+    private String country;
+
+    public Address(String streetAddress, String city, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.country = country;
+    }
+
+    public Address(Address other) {
+        this(other.streetAddress, other.city, other.country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
+class Employee {
+    public String name;
+    public Address address;
+
+    public Employee(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Employee(Employee other) {
+        this(other.name, new Address(other.address));
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
+
+public class CopyConstructor {
+
+    public static void main(String[] args) {
+        Employee luiggi = new Employee("Luiggi", new Address("Chorrillos", "Lima", "Peru"));
+
+
+        Employee chris = new Employee(luiggi);
+        chris.name="Chris";
+
+        System.out.println(luiggi.toString());
+        System.out.println(chris.toString());
+
+
+    }
+}
--- src/test/java/Builder/Evaluate.java
+++ src/test/java/Builder/Evaluate.java
@@ -1,6 +1,5 @@
 package Builder;
 
-import construccion.builder.CodeBuilder;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
